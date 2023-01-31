5b1e302645e4dac7e6947b3031433d71f59d72d4
Troy Johnson <tnj8510@gmail.com>
2023-01-30 23:26:57 -0800

    add db

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,31 @@
-DROP DATABASE IF EXISTS db_name
-CREATE DATABASE db_name
+DROP DATABASE IF EXISTS company_db
+CREATE DATABASE company_db
 
-use db_name
+use company_db
 
-CREATE TABLE farley (
-name VARCHAR(64) DEFAULT "Default Name",
-age INT NOT NULL DEFAULT 18,
-description VARCHAR(64) DEFAULT "Default Description"
+CREATE TABLE department (
+  id INT PRIMARY KEY,
+  name VARCHAR(30)
 );
+
+CREATE TABLE role (
+  id INT PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+  id INT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT,
+  manager_id INT,
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
+
 
 INSET INTO farley(age) VALUES (24)
 
